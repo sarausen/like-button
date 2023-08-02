801b4488ebbe0e6eb36666a42a2b88deabed653b
suusara <smataeva09@gmail.com>
2023-08-02 13:14:25 +0600

    add js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+// Variables
+
+const likeBtn = document.querySelector('.heart-icon');
+const numberOfLikesElement = document.querySelector('.number-of-likes');
+
+let numberOfLikes = Number.parseInt(numberOfLikesElement.textContent, 10);
+let isLiked = false;
+
+// Functions
+
+const likeClick = () => {
+  if (!isLiked) {
+    likeBtn.classList.add('isLiked');
+    numberOfLikes++;
+    numberOfLikesElement.textContent = numberOfLikes;
+    isLiked = !isLiked;
+  } else {
+    likeBtn.classList.remove('isLiked');
+    numberOfLikes--;
+    numberOfLikesElement.textContent = numberOfLikes;
+    isLiked = !isLiked;
+  }
+};
+
+// Event Listeners
+
+likeBtn.addEventListener('click', likeClick);
 
